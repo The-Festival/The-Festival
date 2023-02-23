f2e84341588dc2c32ad8d9e5587c6bb43dadb908
muttalipSahin <66790473+muttalipSahin@users.noreply.github.com>
2023-02-23 13:11:53 +0100

    kids desktop info page finished

---
+++ app/controllers/kidscontroller.php
@@ -0,0 +1,23 @@
+<?php
+class KidsController
+{
+    public function index()
+    {
+        // Detect screen size
+            if(strpos($_SERVER['HTTP_USER_AGENT'], 'Mobile') !== false) {
+                $screenSize = 'mobile';
+            } else {
+                $screenSize = 'desktop';
+            }
+  
+        // Load appropriate file based on screen size
+            if($screenSize === 'mobile') {
+                // load kids app on mobile
+                require __DIR__ . '/../views/kids/mobile/mobileApp.php';
+            } else {
+                // load info page on desktop
+                require __DIR__ . '/../views/kids/desktop/infoDesktop.php';
+            }
+    }
+
+}
---
+++ app/views/kids/mobile/mobileApp.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>KID MOBILE</h1>
+</div>
