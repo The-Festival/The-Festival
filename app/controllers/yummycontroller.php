c02c8eb70d5a5dbb13e548a40d051e22c1d57d2e
Yara Bok <yarabok210804@gmail.com>
2023-03-08 11:49:40 +0100

    navbar klaar denk
    de navbar is sticky

---
+++ app/controllers/yummycontroller.php
@@ -0,0 +1,8 @@
+<?php
+class YummyController
+{
+    public function yummy()
+    {
+        require __DIR__ . '/../views/yummy/yummy.php';
+    }
+}
--- app/views/header.php
+++ app/views/header.php
@@ -4,39 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MVC demo</title>
+    <title>The Festival</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link type="text/css"
         rel = "stylesheet"
-        href = "css/styleNav.css"/> 
+        href = "/css/styleNav.css"/> 
+        <link type="text/css"
+        rel = "stylesheet"
+        href = "/css/footer-style.css"/> 
 </head>
 <body>
  
-<nav class="navbar navbar-expand-lg bg-body-tertiary p-0">
+<nav class="navbar navbar-expand-lg bg-body-tertiary p-0" id="nav">
   <div class="container" id="header">
     <a class="navbar-brand" href="/"> <img  id="logo" src="img/logo.png" /></a>
-    <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button> -->
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav" >
       <ul class=" d-flex justify-content-end navbar-nav me-auto w-100">
-        <li class="nav-item">
-          <a class="nav-link" href="/article">Dance</a></li>
-        <li class="nav-item">
-          <a class="nav-link" href="/home/about">Jazz</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/home/about">Yummy</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/home/about">History</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" ><img  id="language" src="img/taal.png"/></a>
-        </li>
+        <li class="nav-item"><a class="nav-link text-white" href="/article">Dance</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="/home/about">Jazz</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="/yummy/yummy">Yummy</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="/home/about">History</a></li>
+        <li class="nav-item"><a class="nav-link text-white" ><img  id="language" src="img/taal.png"/></a></li>
       </ul>   
         
     </div>
