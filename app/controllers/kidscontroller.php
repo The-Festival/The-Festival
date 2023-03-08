4d4a8d4f18568c122e3e7915b565eb35ae34a2f9
muttalipSahin <66790473+muttalipSahin@users.noreply.github.com>
2023-03-09 00:36:59 +0100

    kids-drfeathers eggproblem almost finished

--- app/controllers/kidscontroller.php
+++ app/controllers/kidscontroller.php
@@ -33,9 +33,36 @@ class KidsController
         require __DIR__ . '/../views/kids/mobile/activitylinkpages/activity6.php';
     }
 
+    public function facttwo(){
+        require __DIR__ . '/../views/kids/mobile/theeggproblem/drfeatherfacts/fact2.php';
+    }
+    public function factthree(){
+        require __DIR__ . '/../views/kids/mobile/theeggproblem/drfeatherfacts/fact3.php';
+    }
+    public function factfour(){
+        require __DIR__ . '/../views/kids/mobile/theeggproblem/drfeatherfacts/fact4.php';
+    }
+    public function factfive(){
+        require __DIR__ . '/../views/kids/mobile/theeggproblem/drfeatherfacts/fact5.php';
+    }
+
+    public function factfinished(){
+        require __DIR__ . '/../views/kids/mobile/theeggproblem/drfeatherfacts/factfinished.php';
+    }
+
     public function theeggproblem(){
         require __DIR__ . '/../views/kids/mobile/theeggproblem/theggproblemMain.php';
     }
 
+    public function drfeathersfound(){
+        require __DIR__ . '/../views/kids/mobile/theeggproblem/drfeathersfound.php';
+    }
 
+    public function eggproblemfacts(){
+        require __DIR__ . '/../views/kids/mobile/theeggproblem/factspageinitial.php';
+    }
+
+    public function eggproblempage(){
+        require __DIR__ . '/../views/kids/mobile/theeggproblem/eggproblem.php';
+    }
 }
---
+++ app/views/kids/mobile/theeggproblem/drfeatherfacts/fact3.php
@@ -0,0 +1,26 @@
+<link href='https://fonts.googleapis.com/css?family=Itim' rel='stylesheet'>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<style>
+<?php include '/app/views/kids/mobile/theeggproblem/theeggproblemStyle.css'; ?>
+</style>
+<body>
+<header>
+<div>
+    <div >
+        <img id="logo" class="" src="/../img/logo.png" alt="mobile design">
+    </div>
+</div>
+</header>
+    <h1>The Egg Problem</h1>
+    <div class = "container">
+    <div class="row mt-5">
+        <div class="col-6"><h1>Fact 3</h1></div>
+        <div class = "col-6"></div>
+    </div>
+    <div class="row mt-5">
+        <div class="col-6"><h2>Did you know that people have been eating eggs for thousands of years? That's a really, really long time!</h2></div>
+        <div class = "col-6"><img id = "drFeathersInverted"src="/../img/Dr.Feathers.png" alt=""></div>
+    </div>
+</div>
+<button class = "button" onclick="window.location.href='/kids/factfour'">Next fact</button>
+</body>
