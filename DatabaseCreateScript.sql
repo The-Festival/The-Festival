69c2261f716338c867e962b7470a284b5e2fdb84
muttalipSahin <66790473+muttalipSahin@users.noreply.github.com>
2023-03-13 20:44:40 +0100

    database creatie script

---
+++ DatabaseCreateScript.sql
@@ -0,0 +1,443 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql
+-- Generation Time: Mar 13, 2023 at 07:42 PM
+-- Server version: 10.10.2-MariaDB-1:10.10.2+maria~ubu2204
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `developmentdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `About`
+--
+
+CREATE TABLE `About` (
+  `about_id` int(11) NOT NULL,
+  `detail_id` int(11) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `about` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Artist`
+--
+
+CREATE TABLE `Artist` (
+  `artist_id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `about` text NOT NULL,
+  `price` float NOT NULL,
+  `link` varchar(128) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `genre` varchar(256) NOT NULL,
+  `track` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Event Jazz`
+--
+
+CREATE TABLE `Event Jazz` (
+  `event_id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `about` text NOT NULL,
+  `general_info` text NOT NULL,
+  `bannerfilepath` varchar(264) NOT NULL,
+  `datetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Foto`
+--
+
+CREATE TABLE `Foto` (
+  `foto_id` int(11) NOT NULL,
+  `detail_id` int(11) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `filepath` varchar(260) NOT NULL,
+  `isBanner` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Guide`
+--
+
+CREATE TABLE `Guide` (
+  `guide_id` int(11) NOT NULL,
+  `tour_id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Language`
+--
+
+CREATE TABLE `Language` (
+  `language_id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `available_spaces` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Location`
+--
+
+CREATE TABLE `Location` (
+  `location_id` int(11) NOT NULL,
+  `detail_id` int(11) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `streetname` varchar(128) NOT NULL,
+  `postalcode` varchar(6) NOT NULL,
+  `city` varchar(64) NOT NULL,
+  `housenumber` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Pass`
+--
+
+CREATE TABLE `Pass` (
+  `pass_id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `description` varchar(64) NOT NULL,
+  `price` float NOT NULL,
+  `startingTime` time NOT NULL,
+  `finishTime` time NOT NULL,
+  `event_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Point of interest`
+--
+
+CREATE TABLE `Point of interest` (
+  `poi_id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Reservation`
+--
+
+CREATE TABLE `Reservation` (
+  `reservation_id` int(11) NOT NULL,
+  `session_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `request` text NOT NULL,
+  `count_people` int(11) NOT NULL,
+  `reservation_fee` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Restraurant`
+--
+
+CREATE TABLE `Restraurant` (
+  `restaurant_id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `description` text NOT NULL,
+  `price` float NOT NULL,
+  `restaurant_type` varchar(128) NOT NULL,
+  `star_rating` int(11) NOT NULL,
+  `cuisine` varchar(128) NOT NULL,
+  `email` varchar(320) NOT NULL,
+  `phonenumber` varchar(15) NOT NULL,
+  `total_seats` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Session`
+--
+
+CREATE TABLE `Session` (
+  `session_id` int(11) NOT NULL,
+  `restaurant_id` int(11) NOT NULL,
+  `start_datetime` datetime NOT NULL,
+  `duration` time NOT NULL,
+  `seats_left` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Tour`
+--
+
+CREATE TABLE `Tour` (
+  `tour_id` int(11) NOT NULL,
+  `language_id` int(11) NOT NULL,
+  `datetime` datetime NOT NULL,
+  `start_location` varchar(150) NOT NULL,
+  `seats_left` int(11) NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `user_id` int(11) NOT NULL,
+  `fullname` int(128) NOT NULL,
+  `email` varchar(320) NOT NULL,
+  `paswword` varchar(128) NOT NULL,
+  `role` varchar(20) NOT NULL,
+  `registration_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `About`
+--
+ALTER TABLE `About`
+  ADD PRIMARY KEY (`about_id`);
+
+--
+-- Indexes for table `Artist`
+--
+ALTER TABLE `Artist`
+  ADD PRIMARY KEY (`artist_id`),
+  ADD KEY `event_id` (`event_id`);
+
+--
+-- Indexes for table `Event Jazz`
+--
+ALTER TABLE `Event Jazz`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indexes for table `Foto`
+--
+ALTER TABLE `Foto`
+  ADD PRIMARY KEY (`foto_id`);
+
+--
+-- Indexes for table `Guide`
+--
+ALTER TABLE `Guide`
+  ADD PRIMARY KEY (`guide_id`),
+  ADD KEY `tour_id` (`tour_id`);
+
+--
+-- Indexes for table `Language`
+--
+ALTER TABLE `Language`
+  ADD PRIMARY KEY (`language_id`);
+
+--
+-- Indexes for table `Location`
+--
+ALTER TABLE `Location`
+  ADD PRIMARY KEY (`location_id`);
+
+--
+-- Indexes for table `Pass`
+--
+ALTER TABLE `Pass`
+  ADD PRIMARY KEY (`pass_id`),
+  ADD KEY `event_id` (`event_id`);
+
+--
+-- Indexes for table `Point of interest`
+--
+ALTER TABLE `Point of interest`
+  ADD PRIMARY KEY (`poi_id`);
+
+--
+-- Indexes for table `Reservation`
+--
+ALTER TABLE `Reservation`
+  ADD PRIMARY KEY (`reservation_id`),
+  ADD KEY `session_id` (`session_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `Restraurant`
+--
+ALTER TABLE `Restraurant`
+  ADD PRIMARY KEY (`restaurant_id`);
+
+--
+-- Indexes for table `Session`
+--
+ALTER TABLE `Session`
+  ADD PRIMARY KEY (`session_id`),
+  ADD KEY `restaurant_id` (`restaurant_id`);
+
+--
+-- Indexes for table `Tour`
+--
+ALTER TABLE `Tour`
+  ADD PRIMARY KEY (`tour_id`),
+  ADD KEY `language_id` (`language_id`);
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `About`
+--
+ALTER TABLE `About`
+  MODIFY `about_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Artist`
+--
+ALTER TABLE `Artist`
+  MODIFY `artist_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Event Jazz`
+--
+ALTER TABLE `Event Jazz`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Guide`
+--
+ALTER TABLE `Guide`
+  MODIFY `guide_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Language`
+--
+ALTER TABLE `Language`
+  MODIFY `language_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Location`
+--
+ALTER TABLE `Location`
+  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Pass`
+--
+ALTER TABLE `Pass`
+  MODIFY `pass_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Point of interest`
+--
+ALTER TABLE `Point of interest`
+  MODIFY `poi_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Reservation`
+--
+ALTER TABLE `Reservation`
+  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Restraurant`
+--
+ALTER TABLE `Restraurant`
+  MODIFY `restaurant_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Session`
+--
+ALTER TABLE `Session`
+  MODIFY `session_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Tour`
+--
+ALTER TABLE `Tour`
+  MODIFY `tour_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Artist`
+--
+ALTER TABLE `Artist`
+  ADD CONSTRAINT `Artist_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `Event Jazz` (`event_id`);
+
+--
+-- Constraints for table `Guide`
+--
+ALTER TABLE `Guide`
+  ADD CONSTRAINT `Guide_ibfk_1` FOREIGN KEY (`tour_id`) REFERENCES `Tour` (`tour_id`);
+
+--
+-- Constraints for table `Pass`
+--
+ALTER TABLE `Pass`
+  ADD CONSTRAINT `Pass_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `Event Jazz` (`event_id`);
+
+--
+-- Constraints for table `Reservation`
+--
+ALTER TABLE `Reservation`
+  ADD CONSTRAINT `Reservation_ibfk_1` FOREIGN KEY (`session_id`) REFERENCES `Session` (`session_id`),
+  ADD CONSTRAINT `Reservation_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`);
+
+--
+-- Constraints for table `Session`
+--
+ALTER TABLE `Session`
+  ADD CONSTRAINT `Session_ibfk_1` FOREIGN KEY (`restaurant_id`) REFERENCES `Restraurant` (`restaurant_id`);
+
+--
+-- Constraints for table `Tour`
+--
+ALTER TABLE `Tour`
+  ADD CONSTRAINT `Tour_ibfk_1` FOREIGN KEY (`language_id`) REFERENCES `Language` (`language_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
