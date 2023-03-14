6300f5918e10306a7e7054a76463d8d5c8d033c2
l-huisman <64755533+l-huisman@users.noreply.github.com>
2023-03-14 12:41:41 +0100

    Dashboard added buttons to load dashboard dynamically

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -3,6 +3,11 @@ $var = 'dashboard';
 include __DIR__ . '/../header.php';
 ?>
 
+<div class="row dashboard-button-background">
+    <button type="" name="submitButton" value="" class="col active text-light dashboard-button ">Jazz</button>
+    <button type="" name="submitButton" value="" class="col text-light dashboard-button ">Yummy</button>
+    <button type="" name="submitButton" value="" class="col text-light dashboard-button ">History</button>
+</div>
 <div class="container-lg">
     <div class="row">
         <div class="col mt-3">
@@ -22,4 +27,32 @@ include __DIR__ . '/../header.php';
         box-shadow: 10px 10px 4px rgba(0, 0, 0, 0.25);
         border-radius: 0px;
     }
+
+    .dashboard-button {
+        background: none;
+        border: 0;
+        height: 3em;
+    }
+
+    .active {
+        background-color: rgba(0, 0, 0, 0.25);
+    }
+
+    .dashboard-button-background {
+        background: #6716C7;
+    }
 </style>
+
+<script>
+    var buttons = document.getElementsByClassName('dashboard-button');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function() {
+            for (var j = 0; j < buttons.length; j++) {
+                if (buttons[j] !== this && buttons[j].classList.contains('active')) {
+                    buttons[j].classList.remove('active');
+                }
+            }
+            this.classList.add('active');
+        });
+    }
+</script>
