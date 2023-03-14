9842c2a84e1a0fa2725df7d52e70d8cd01b6faec
l-huisman <64755533+l-huisman@users.noreply.github.com>
2023-03-14 12:05:30 +0100

    Added card to the dashboard

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -3,3 +3,23 @@ $var = 'dashboard';
 include __DIR__ . '/../header.php';
 ?>
 
+<div class="container-lg">
+    <div class="row">
+        <div class="col mt-3">
+            <div class="card card-background d-flex align-items-center">
+                <h4 class="card-title"> List of Artists </h4>
+                <div class="card-body">
+                    {Artist name} <button class="btn btn-danger">Delete</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .card-background {
+        background: rgba(255, 251, 251, 0.3);
+        box-shadow: 10px 10px 4px rgba(0, 0, 0, 0.25);
+        border-radius: 0px;
+    }
+</style>
