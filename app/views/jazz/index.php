62957009867f228ed469d31c0323f75e99b88151
l-huisman <64755533+l-huisman@users.noreply.github.com>
2023-03-14 17:19:08 +0100

    Finished the artist card

--- app/views/jazz/index.php
+++ app/views/jazz/index.php
@@ -19,7 +19,12 @@
             <h2 class="intro-title">Haarlem Jazz</h2>
             <h2 class="intro-title">27 July - 30 July</h2>
         </div>
-        <p class="intro-paragraph">The Haarlem Jazz Festival is a four-day celebration of jazz music, taking place from July 27th to July 30th in the historic city of Haarlem. Set against the stunning backdrop of Haarlem's historic architecture and picturesque canals, the festival brings together some of the biggest names in jazz, as well as up-and-coming talent, for a series of concerts, workshops, and jam sessions. With a diverse lineup of international and local artists, the Haarlem Jazz Festival is a must-attend event for any jazz enthusiast.</p>
+        <p class="intro-paragraph">The Haarlem Jazz Festival is a four-day celebration of jazz music, taking place from
+            July 27th to July 30th in the historic city of Haarlem. Set against the stunning backdrop of Haarlem's
+            historic architecture and picturesque canals, the festival brings together some of the biggest names in
+            jazz, as well as up-and-coming talent, for a series of concerts, workshops, and jam sessions. With a diverse
+            lineup of international and local artists, the Haarlem Jazz Festival is a must-attend event for any jazz
+            enthusiast.</p>
     </article>
 
     <!-- 5 buttons to represent the artist selection -->
@@ -33,35 +38,97 @@
         </div>
     </div>
 
+    <!-- Artist cards -->
+    <div class="container">
+        <div class="row gap-3 my-3 d-flex justify-content-center">
+            <div class="card p-0 col d-flex align-items-center justify-content-center artist-card">
+                <div class="card-title g-0 w-100 artist-card-title">Miles Sanko</div>
+                <div class="card-body w-100 d-flex align-self-start flex-column artist-card-body">
+                    <img class="artist-image mb-3 align-self-center" src="/img/artist-subhero.png" alt="Img">
+                    <p><strong>Time:</strong> 20:00 - 22:00</p>
+                    <p><strong>Location:</strong> Patronaat</p>
+                    <p><strong>Hall:</strong> Main Hall</p>
+                    <p><strong>Price:</strong> €15</p>
+                    <a href="#">Discover more!</a>
+                    <button class="btn btn-primary mt-3 artist-card-btn w-100">Add to Cart +</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- End -->
+
+    <style>
+        .artist-card {
+            background: rgba(255, 255, 255, 0.6);
+            border: 7px solid #000000;
+            max-width: 300px;
+        }
+
+        .artist-card-title {
+            background: #6716C7;
+            border-bottom: 7px solid #000000;
+            font-family: 'Lobster Two';
+            font-weight: 400;
+            font-size: 37px;
+            text-align: center;
+            color: #FFFFFF;
+        }
+
+        .artist-card-btn {
+            background: #0C60DD;
+            box-shadow: 4px 4px 6px rgba(0, 0, 0, 0.25), inset 4px 4px 7px rgba(255, 255, 255, 0.25);
+            border-radius: 20px;
+        }
+
+        .artist-image {
+            width: 100%;
+            max-width: 200px;
+            aspect-ratio: 1/1;
+            object-fit: cover;
+            border-radius: 50%;
+        }
+
+        .artist-card-body {
+            font-family: 'Poppins';
+            font-size: 1.25rem;
+            line-height: 100%;
+        }
+    </style>
+
     <!-- 4 Cards that represents the passes -->
 
-    <div class="container">
-        <div class="row gap-5">
+    <div class="container mb-3">
+        <div class="row gap-5 d-flex justify-content-center">
             <div class="card col-sm card-wrapper">
                 <div class="card-body">
                     <h4 class="card-heading">Day pass <br> Thursday</h4>
-                    <p class="card-paragraph">All-Access pass for <br> Thursday <br> Time: 18:00 - 22:00 <br> Price: €35</p>
+                    <p class="card-paragraph">All-Access pass for <br> Thursday <br> Time: 18:00 - 22:00 <br> Price: €35
+                    </p>
                     <button class="btn btn-primary card-button">Add to Cart +</button>
                 </div>
             </div>
             <div class="card col-sm card-wrapper">
                 <div class="card-body">
                     <h4 class="card-heading">Day pass <br> Friday</h4>
-                    <p class="card-paragraph">All-Access pass for <br> Friday <br> Time: 18:00 - 22:00 <br> Price: €35</p>
+                    <p class="card-paragraph">All-Access pass for <br> Friday <br> Time: 18:00 - 22:00 <br> Price: €35
+                    </p>
                     <button class="btn btn-primary card-button">Add to Cart +</button>
                 </div>
             </div>
             <div class="card col-sm card-wrapper">
                 <div class="card-body">
                     <h4 class="card-heading">Day pass <br> Saturday</h4>
-                    <p class="card-paragraph">All-Access pass for <br> Saturday <br> Time: 18:00 - 22:00 <br> Price: €35</p>
+                    <p class="card-paragraph">All-Access pass for <br> Saturday <br> Time: 18:00 - 22:00 <br> Price: €35
+                    </p>
                     <button class="btn btn-primary card-button">Add to Cart +</button>
                 </div>
             </div>
             <div class="card col-sm card-wrapper">
                 <div class="card-body">
                     <h4 class="card-heading">All-access <br> pass</h4>
-                    <p class="card-paragraph">All-Access pass for <br> Thu-Fri-Sat-Sun. <br> Time: 18:00 - 22:00 <br> Price: €80</p>
+                    <p class="card-paragraph">All-Access pass for <br> Thu-Fri-Sat-Sun. <br> Time: 18:00 - 22:00 <br>
+                        Price: €80</p>
                     <button class="btn btn-primary card-button">Add to Cart +</button>
                 </div>
             </div>
