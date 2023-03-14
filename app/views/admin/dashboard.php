1f8974f46b2db54f84029a588615106e7cb3d807
l-huisman <64755533+l-huisman@users.noreply.github.com>
2023-03-14 13:02:00 +0100

    Dashboard loads views dynamically now

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -3,23 +3,23 @@ $var = 'dashboard';
 include __DIR__ . '/../header.php';
 ?>
 
+<!-- Buttons to switch what dashboard the admin wants to see -->
+
 <div class="row dashboard-button-background">
-    <button type="" name="submitButton" value="" class="col active text-light dashboard-button ">Jazz</button>
-    <button type="" name="submitButton" value="" class="col text-light dashboard-button ">Yummy</button>
-    <button type="" name="submitButton" value="" class="col text-light dashboard-button ">History</button>
+    <button class="col active text-light dashboard-button ">Jazz</button>
+    <button class="col text-light dashboard-button ">Yummy</button>
+    <button class="col text-light dashboard-button ">History</button>
 </div>
-<div class="container-lg">
-    <div class="row">
-        <div class="col mt-3">
-            <div class="card card-background d-flex align-items-center">
-                <h4 class="card-title"> List of Artists </h4>
-                <div class="card-body">
-                    {Artist name} <button class="btn btn-danger">Delete</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
+<!-- End -->
+
+<!-- This part of the dashboard needs to be dynamically loaded depending on what button is active -->
+
+<div id="dashboard-content"></div>
+
+<!-- End -->
+
+<!-- This part needs to go to it's own file in the future -->
 
 <style>
     .card-background {
@@ -34,25 +34,47 @@ include __DIR__ . '/../header.php';
         height: 3em;
     }
 
-    .active {
-        background-color: rgba(0, 0, 0, 0.25);
-    }
-
     .dashboard-button-background {
         background: #6716C7;
+    }
+
+    .active {
+        background-color: rgba(0, 0, 0, 0.25);
     }
 </style>
 
 <script>
     var buttons = document.getElementsByClassName('dashboard-button');
+    var dashboardContent = document.getElementById('dashboard-content');
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function() {
+            // Remove the "active" class from all buttons except the clicked one
             for (var j = 0; j < buttons.length; j++) {
                 if (buttons[j] !== this && buttons[j].classList.contains('active')) {
                     buttons[j].classList.remove('active');
                 }
             }
+            // Add the "active" class to the clicked button
             this.classList.add('active');
+
+            // Update the content of the dashboard based on the clicked button
+            var dashboardContentText;
+            switch (this.innerText) {
+                case 'Jazz':
+                    dashboardContentText = '<h2>Jazz Dashboard</h2>';
+                    break;
+                case 'Yummy':
+                    dashboardContentText = '<h2>Yummy Dashboard</h2>';
+                    break;
+                case 'History':
+                    dashboardContentText = '<h2>History Dashboard</h2>';
+                    break;
+                default:
+                    dashboardContentText = '<h2>Unknown Dashboard!<br>Please try to log in again</h2>';
+            }
+            dashboardContent.innerHTML = dashboardContentText;
         });
     }
 </script>
+
+<!-- End -->
