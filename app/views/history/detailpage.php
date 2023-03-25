913b1c37b4243aaa9c9e121d8e8c0522b8973e3a
ThijsMoerland <Moerland8@gmail.com>
2023-03-25 16:57:24 +0100

    detail page css

--- app/views/history/detailpage.php
+++ app/views/history/detailpage.php
@@ -2,7 +2,14 @@
 $var = 'history';
 require __DIR__ . '/../header.php';
 ?>
-<div class="hero-image" id="hero-image"></div>
+<div class="hero-image mb-3" id="hero-image"></div>
+
+<?php foreach($pagePOI as $poi) { ?>
+    <div class="detailSection containter w-75 mx-auto mb-5">
+        <p class="detailTXT"><?php echo $poi->getText(); ?></p>
+        <img src="<?php echo $poi->getPhoto(); ?>" alt="image" class="detailIMG">
+    </div>
+<?php } ?>
 
 <?php 
 require __DIR__ . '/../footer/footer.php';
