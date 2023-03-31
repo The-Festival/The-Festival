c10376518b92df5cabe1305a4f91552ad18ac230
ThijsMoerland <Moerland8@gmail.com>
2023-03-31 09:39:19 +0200

    mutti location.php

--- app/models/Location.php
+++ app/models/Location.php
@@ -1,77 +1,29 @@
 <?php
 
-class Location{
+class Location {
     private $location_id;
     private $detail_id;
-    private $type;
-    private $place_name;
     private $streetname;
     private $postalcode;
     private $city;
     private $housenumber;
-
-
-    public function getLocation_id(){
+    
+    public function getLocationID(){
         return $this->location_id;
     }
-
-    public function getDetail_id(){
+    public function getPOIID(){
         return $this->detail_id;
     }
-
-    public function getType(){
-        return $this->type;
-    }
-
-    public function getPlace_name(){
-        return $this->place_name;
-    }
-
-    public function getStreetname(){
+    public function getStreetName(){
         return $this->streetname;
     }
-
-    public function getPostalcode(){
+    public function getPostalCode(){
         return $this->postalcode;
     }
-
     public function getCity(){
         return $this->city;
     }
-
     public function getHousenumber(){
         return $this->housenumber;
-    }
-
-    public function setLocation_id($location_id){
-        $this->location_id = $location_id;
-    }
-
-    public function setDetail_id($detail_id){
-        $this->detail_id = $detail_id;
-    }
-
-    public function setType($type){
-        $this->type = $type;
-    }
-
-    public function setPlace_name($place_name){
-        $this->place_name = $place_name;
-    }
-
-    public function setStreetname($streetname){
-        $this->streetname = $streetname;
-    }
-
-    public function setPostalcode($postalcode){
-        $this->postalcode = $postalcode;
-    }
-
-    public function setCity($city){
-        $this->city = $city;
-    }
-
-    public function setHousenumber($housenumber){
-        $this->housenumber = $housenumber;
     }
 }
