312a09a2fe1b3a41361d029b5b84240508c2810b
l-huisman <huisman.l@yahoo.com>
2023-04-06 13:41:34 +0200

    Added tracking

---
+++ app/controllers/qrcontroller.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+class QrController
+{
+    public function index()
+    {
+        $this->generateQRCode();
+    }
+
+    public function generateQRCode()
+    {
+        
+    }
+}
