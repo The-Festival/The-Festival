96d6f5e1a39e833e561ce98cd70a780b1740853b
l-huisman <64755533+l-huisman@users.noreply.github.com>
2023-04-12 21:08:57 +0200

    QR code generator is now working

--- app/controllers/qrcontroller.php
+++ app/controllers/qrcontroller.php
@@ -14,49 +14,41 @@ use Endroid\QrCode\Writer\ValidationException;
 
 class QrController
 {
-    public function index()
+    private $data;
+    private $writer;
+
+    function index()
     {
-        $this->renderQRCode($this->generateQRCode());
+        $this->writer = new PngWriter();
+        $this->generateQRCode();
     }
 
-    public function generateQRCode()
+    function generateQRCode()
     {
-        $writer = new PngWriter();
-
+        !isset($_GET['link']) ? $qrLink = "http://62.250.182.52/" : $qrLink = $_GET['link'];
+        
         // Create QR code
-        $qrCode = QrCode::create('Life is too short to be generating QR codes')
+        $qrCode = QrCode::create($qrLink)
             ->setEncoding(new Encoding('UTF-8'))
             ->setErrorCorrectionLevel(new ErrorCorrectionLevelLow())
-            ->setSize(300)
+            ->setSize(500)
             ->setMargin(10)
             ->setRoundBlockSizeMode(new RoundBlockSizeModeMargin())
             ->setForegroundColor(new Color(0, 0, 0))
             ->setBackgroundColor(new Color(255, 255, 255));
 
-        // Create generic logo
-        $logo = Logo::create(__DIR__ . '/assets/symfony.png')
-            ->setResizeToWidth(50);
-
         // Create generic label
-        $label = Label::create('Label')
-            ->setTextColor(new Color(255, 0, 0));
+        $label = Label::create("Scan me")
+            ->setTextColor(new Color(0, 0, 0));
 
-        $result = $writer->write($qrCode, $logo, $label);
+        $result = $this->writer->write($qrCode, null, $label);
 
-        // Validate the result
-        $writer->validateResult($result, 'Life is too short to be generating QR codes');
-
-        return $result;
-    }
-
-    public function renderQRCode($result)
-    {
         // Directly output the QR code
         header('Content-Type: ' . $result->getMimeType());
         echo $result->getString();
 
         // Save it to a file
-        $result->saveToFile(__DIR__ . '/qrcode.png');
+        $result->saveToFile(__DIR__ . '/public/img/qr/qrcode.png');
 
         // Generate a data URI to include image data inline (i.e. inside an <img> tag)
         $dataUri = $result->getDataUri();
